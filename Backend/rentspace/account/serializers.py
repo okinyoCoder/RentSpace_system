3300943f5914c623492e64d8df0f1324ed65c809
OkinyoCoder <anthonyodhiambo97@gmail.com>
2025-06-25 12:23:13 +0300

    created account serializer and views

--- Backend/rentspace/account/serializers.py
+++ Backend/rentspace/account/serializers.py
@@ -1,1 +1,37 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """User serializer"""
+    class Meta:
+        password = serializers.CharField(
+            write_only=True, required=True, validators=[validate_password]
+        )
+        
+        model = User
+        fields = ['id', 'email', 'username', 'phone_number', 'user_type', 'profile_picture', 'password','password1','date_joined']
+        read_only_fields =[ 'id', 'date_joined']
+        
+        def validate_email(self, value):
+            if User.objects.filter(email=value).exists():
+                raise serializers.ValidationError("Email already in Use")
+            return value
+        
+        def validate(self, data):
+            if data['password'] != data['password1']:
+                raise serializers.ValidationError("password must match")
+            validate_password(data['password'])
+            return data
+            
+                    
+        def create(self, validated_data):
+            validated_data.pop('password1')
+            password = validated_data.pop('password')
+            user = User(**validated_data)
+            user.set_password(password)
+            user.save()
+            return user
--- Backend/rentspace/account/views.py
+++ Backend/rentspace/account/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from serializers import UserSerializer
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import get_user_model
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(APIView):
+    """User registration views"""
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    """user login views"""
+    def post(self, request):
+        email = request.data.get["email"]
+        password = request.data.get["password"]
+        
+        try:
+            validate_email(email)
+        except ValidationError:
+            return Response({'error': 'Enter valid email address'},
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
+        user = authenticate(request, username=email, password=password)
+        if user:
+            return Response({"message":"Login successful."})
+        return Response({"error": "invalid email or password"},  status=status.HTTP_401_UNAUTHORIZED)     
