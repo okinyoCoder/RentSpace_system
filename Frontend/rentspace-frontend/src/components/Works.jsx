91e1ac8df0a3896d0beb2c3d8b46e474ed5766c6
okinyoCoder <okinyoanthony@gmail.com>
2025-06-27 14:11:52 +0300

    created a homepage

---
+++ Frontend/rentspace-frontend/src/components/Works.jsx
@@ -0,0 +1,41 @@
+import './works.scss'
+
+function Works() {
+    return(
+        <div className="workcontent">
+            <div className="textcont">
+                <h2>
+                    How it works?
+                </h2>
+                <p>
+                    How does Rentspace Finder works?
+                    This works by connecting tenants to their desired real estate.
+                    As a results, landlords are able to get tenants and tenants are 
+                    able to choose from a pool of real estate
+                </p>
+            </div>
+            <div className="textcont dist">
+                <div className="colum">
+                    <img src="" alt="" />
+                    <h3>Search Property</h3>
+                    <p></p>
+                    <a href="">Know More</a>
+                </div>
+                <div className="colum">
+                    <img src="" alt="" />
+                    <h3>Choose Property</h3>
+                    <p></p>
+                    <a href="">Know More</a>
+                </div>
+                <div className="colum">
+                    <img src="" alt="" />
+                    <h3>Rent Property</h3>
+                    <p></p>
+                    <a href="">Know More</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Works
--- Frontend/rentspace-frontend/src/routes/homepage.jsx
+++ Frontend/rentspace-frontend/src/routes/homepage.jsx
@@ -1,10 +1,40 @@
 import './homepage.jsx'
+import Works from '../components/Works.jsx'
 
 function Homepage() {
     return(
         <div className="homepage">
-            <div className="textcontent"></div>
-            <div className="imgcontent"></div>
+            <div className="textcontent">
+                <h2>Find <span> Perfect </span>
+                    Place To Live Life
+                </h2>
+                <div className="subcontent">
+                    <div className="subcontent">
+                        <img src="" alt="" />
+                        <div className="subdetails">
+                            <h3>I'm looking for rental house</h3>
+                            <p>Provide a rental house listing where tenants select
+                                desired property to move into
+                            </p>
+                        </div>
+                        <button>Get House</button>
+                    </div>
+                    <div className="subcontent">
+                        <img src="" alt="" />
+                        <div className="subdetails">
+                            <h3>I want to rent my house</h3>
+                            <p>Here's how to rent your house from setting rent to
+                                screening tenants. How to rent your House
+                            </p>
+                        </div>
+                        <button>Rent House</button>                       
+                    </div>
+                </div>
+                <Works />
+            </div>
+            <div className="imgcontent">
+                <img src="/src/assets/p.2.jpg" alt="" />
+            </div>
         </div>
 
     )
