d2c12b71df0798f15ff168b76a6f4e9c270ea0cf
OkinyoCoder <anthonyodhiambo97@gmail.com>
2025-07-09 07:17:52 +0700

    updated backend

--- Backend/rentspace/rentspace/settings.py
+++ Backend/rentspace/rentspace/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'rentspace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rentspace_db',        
+        'USER': 'root',     
+        'PASSWORD': 'Root#254',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
